78fc038a27dc48571f676d1652f6cf1a717f82a1
tamlt2704 <tamlt2704@gmail.com>
2020-06-02 13:51:20 +0800

    1. Hello world

---
+++ index.js
@@ -0,0 +1,7 @@
+const express = require('express')
+const app = express()
+const port = 5000
+
+app.get('/', (req, res) => res.send('Hello world'))
+
+app.listen(port, () => console.log(`Example app running at http://localhost:${port}`))
